78344ef7385c90437f07a2b9a8b56d7c999e690d
alvinwahyupratama2412 <alvinwahyupratama22@gmail.com>
2025-06-10 10:39:18 +0700

    membuka file dalam mode menulis dan menunjuk ke sebuah nama file

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -8,5 +8,8 @@ int main(){
 
     cout << "Masukkan Nama file : ";
     cin >> NamaFile;
-    
+    //membuka file dalam mode menulis
+    ofstream outfile;
+    //menunjuk ke sebuah nama file
+    outfile.open(NamaFile + "txt", ios::out);
 }
