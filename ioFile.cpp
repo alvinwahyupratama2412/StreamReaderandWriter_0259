15a3ac44a7eb76f4cedbb76e4f11126be70a48a5
alvinwahyupratama2412 <alvinwahyupratama22@gmail.com>
2025-06-10 10:32:40 +0700

    membuat conditional steatment jika file ada dan tidak ada

--- ioFile.cpp
+++ ioFile.cpp
@@ -30,4 +30,17 @@ int main(){
     ifstream infile;
     // menunjuk ke sebuah file
     infile.open("contohfile.txt");
+
+    cout << endl<< ">= Membuka dan membaca file " << endl;
+    //jika file ada maka
+    if(infile.is_open()){
+        //melalukan perulangan setiap baris 
+        while (getline(infile, baris)){
+            //dan tampilkan di sini
+            cout << baris << '\n';
+        }
+    }
+    //jika tidak ditemukan file maka akan menampilkan :
+    else cout << "unable to open file";
+    return 0;
 }
