6cb8861eb913eab61161c7caea553b25b41cdcaf
alvinwahyupratama2412 <alvinwahyupratama22@gmail.com>
2025-06-10 11:15:55 +0700

    membuat penenda 1 dan int 10 sampai 30

---
+++ exeptionStandard.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+//untuk obyek exeption yang akan digunakan
+#include <array>
+//untuk obyek array yang akan kita gunakan
+using namespace std;
+
+int main (){
+    cout << "Awal program" << endl; //penanda 1:...
+    try{
+        array <int, 3> data = {10,20,30};
+        //int data [3] = {10,20,30}
+        //pesan array integer 3 3l3m3n
+        cout << data.at(5) << endl;
+        // memanggil array element ke 5
+    }
+   
+}
